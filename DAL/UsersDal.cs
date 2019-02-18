c37050e2f93af904f735ff9f8e3fa7d09cfa6cb9
宣布 <@DESKTOP-T9DFCI8>
2019-02-18 20:50:29 +0800

    用户信息

---
+++ DAL/UsersDal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MODEL;
+namespace DAL
+{
+    public class UsersDal
+    {
+        PetStoreEntities db = new PetStoreEntities();
+        /// <summary>
+        /// 注册用户
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public int Add(Users model)
+
+        {
+            db.Users.Add(model);
+            return db.SaveChanges();
+        }
+        /// <summary>
+        /// 登陆
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public Users Login(Users model)
+        {
+            var jie= db.Users.Where(m => m.UsersName == model.UsersName && m.UsersPwd == model.UsersPwd).FirstOrDefault();
+            if (jie != null)
+            {
+                return model;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
