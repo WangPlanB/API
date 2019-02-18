1e7156c7cef197bcd3e593b0eb8e7343cc99b50e
1092469189 <1092469189@qq.com>
2019-02-18 20:48:09 +0800

    Cart

---
+++ BLL/CartBLL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class CartBLL
+    {
+        CartDAL dal = new CartDAL();
+        //显示所有购物车信息
+        public List<Cart> ShowCart()
+        {
+            return dal.ShowCart();
+        }
+        //添加购物车信息
+        public int AddCart(Cart mo)
+        {
+            return dal.AddCart(mo);
+        }
+        //删除购物车信息
+        public int DelCart(int Id)
+        {
+            return dal.DelCart(Id);
+        }
+        //修改购物车存储量
+        public int UpdCart(Cart mo)
+        {
+            return dal.UpdCart(mo);
+        }
+    }
+}
